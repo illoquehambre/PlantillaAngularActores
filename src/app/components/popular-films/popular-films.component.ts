f6494692a2a8ec2d95b4ca624fb95f1ca2961019
illoquehambre <illoquehambre@gmail.com>
2022-11-09 08:55:41 +0100

    popularList films done

--- src/app/components/popular-films/popular-films.component.ts
+++ src/app/components/popular-films/popular-films.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Film } from 'src/app/interfaces/filmList.interface';
 import { MoviesService } from 'src/app/services/movies.service';
 import { environment } from 'src/environments/environment';
@@ -14,6 +14,16 @@ export class PopularFilmsComponent implements OnInit {
   //filmSelected: FilmDetailsResponse = {} as FilmDetailsResponse
   numPages=0
   actualPage=1
+
+  @Input()
+  get color(): string {
+    return this._color;
+  }
+  set color(color: string) {
+    this._color = color !== "light" && color !== "dark" ? "light" : color;
+  }
+  private _color = "light";
+
 
   constructor(
     private filmService: MoviesService,
@@ -34,8 +44,8 @@ export class PopularFilmsComponent implements OnInit {
     return new Array(this.numPages)
   }
   
-  showImg(perfil: null|string ) {    
-    return `${environment.posterPath}${perfil}`;
+  showImg(poster: Film) {    
+    return `${environment.FILM_IMG_URL}${poster.poster_path}`;
   }
   /*
 getFilmInfo(film: Film) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://api.themoviedb.org/3',
   apiKey: '1b6406451878df6f600f090780cbeeef',
-  posterPath: 'https://image.tmdb.org/t/p'
+  posterPath: 'https://image.tmdb.org/t/p',
+  FILM_IMG_URL: 'https://image.tmdb.org/t//p/w500/'
 };
